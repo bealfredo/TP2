caca5b55b32bb70a43de10dd7ea824ec53d60134
aguiaralfredo <alfred00a@outlook.com>
2024-03-14 18:35:41 -0300

    angular

--- angular-psicologia/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
-import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
-
-export const routes: Routes = [
-    { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
-    { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estados'},
-];
--- requisitos.md
+++
@@ -1,9 +0,0 @@
-# Requisitos funcionais
-
-## Usuarios
-- FN01: O sistema deve permitir cadastrar usuários com os seguintes dados básicos: login, e-mail, nome, senha
-- FN02: O sistema deve permitir que o usuário consigo gerenciar seu perfil
-## Sistema
-
-# Requisitos Não-Funcionais
-
