fb31cea84094f02b526480cbadf90f7760e7877b
aguiaralfredo <alfred00a@outlook.com>
2024-05-23 17:54:17 -0300

    interceptors, login

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+export type keyLocalStorage = 'jwt_token' | 'usuario_logado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  getItem(key: keyLocalStorage): any {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
+
+  setItem(key: keyLocalStorage, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  removeItem(key: keyLocalStorage): void {
+    localStorage.removeItem(key);
+  }
+
+}
